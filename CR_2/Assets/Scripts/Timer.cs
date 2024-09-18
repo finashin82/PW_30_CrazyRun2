f3a658c72e154bd36159db8d3b59eaa4b967c702
finashin82 <finashin82@mail.ru>
2024-09-18 18:28:47 +0900

    Два уровня
    Два уровня

---
+++ CR_2/Assets/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private int maxTime;
+
+    [SerializeField] private UnityEvent<Timer> timeEvent;
+    public float CurrentTime => currentTime;
+    public float MaxTime => maxTime;
+    public bool IsTimeForGame => currentTime > 0; 
+
+    private float currentTime;   
+
+    private void Awake()
+    {
+        currentTime = maxTime;       
+    }
+
+    void Update()
+    {
+        if (currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+
+            timeEvent?.Invoke(this);
+        }    
+    }
+}
